8a27e2ffc7ccf29f6292b019af0a7795bb34ddcd
Emmanuel Corrales <contact@emcorrales.com>
2019-02-04 20:17:12 -0800

    Do exercise 6.

---
+++ exer6.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Exercise 6:
+#
+# Write a shell script that prompts the user for a name of a file or directory
+# and reports if it is a regular file, a directory, or other type of file. Also
+# perform an ls command against the file or directory with the long listing
+# option.
+
+read -p "Please enter the path of a file or directory: "  FILE
+
+if [ ! -e $FILE ]; then
+  echo "File does not exist!"
+else
+  if [ -f $FILE ]; then
+    TYPE="regular"
+  elif [ -d $FILE ]; then
+    TYPE="directory"
+  fi
+
+  echo "$FILE is a $TYPE file."
+  ls -l $FILE
+fi
+
