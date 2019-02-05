73e783b72ea7316359e1b069380a680286001e00
Emmanuel Corrales <contact@emcorrales.com>
2019-02-04 20:21:19 -0800

    Finish exercise 7.

---
+++ exer7.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Exercise 7:
+#
+# Modify the previous script so that it accepts the file or directory name as an
+# argument instead of prompting the user to enter it.
+
+FILE=$1
+
+if [ ! -e $FILE ]; then
+  echo "File does not exist!"
+else
+  if [ -f $FILE ]; then
+    TYPE="regular"
+  elif [ -d $FILE ]; then
+    TYPE="directory"
+  fi
+
+  echo "$FILE is a $TYPE file."
+  ls -l $FILE
+fi
+
