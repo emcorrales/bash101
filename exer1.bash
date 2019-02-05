269733d16362b559094febe9a37269951a7b3c58
Emmanuel Corrales <contact@emcorrales.com>
2019-02-04 19:18:39 -0800

    Add instructions as comments.

--- exer1.bash
+++ exer1.bash
@@ -1,2 +1,8 @@
 #!/bin/bash
+
+# Exercise 1
+# Write a shell script that prints "Shell Scripting is Fun!" to the screen.
+# Hint 1: Remember to make the shell script executable with the chmod command.
+# Hint 2: Remember to start your script with a shebang!
+
 echo "Shell Scripting is Fun!"
